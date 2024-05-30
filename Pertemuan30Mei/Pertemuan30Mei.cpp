15f85931f1d0b5851fa7899e0701f05d6c4205de
Synzenn <velriel42@gmail.com>
2024-05-30 11:50:38 +0700

    membuat case 1 dan 2

--- Pertemuan30Mei/Pertemuan30Mei.cpp
+++ Pertemuan30Mei/Pertemuan30Mei.cpp
@@ -111,6 +111,16 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
+			switch (ch) {
+			case'1': {
+				q.insert();
+				break;
+			}
+			case'2': {
+				q.remove();
+				break;
+			}
+			}
 	}
 	return 0;
 }
